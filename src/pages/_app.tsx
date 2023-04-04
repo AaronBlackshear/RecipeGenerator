a670ab1c270a73008ae505507aacff75f3171bb0
Aaron Blackshear <ablackshear7820@gmail.com>
2023-04-03 19:28:04 -0600

    Adding base shared styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,20 @@ import type { AppProps } from 'next/app'
 import { UserProvider } from '@auth0/nextjs-auth0/client'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import { Page } from '@components/Page'
-
+import { Nunito_Sans } from 'next/font/google'
 import '@/styles/globals.css'
+
+const nunitoSans = Nunito_Sans({ weight: ["300", "400", "600", "700", "800"], subsets: ['latin'], style: ['normal', 'italic'], })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Page>
-        <Component {...pageProps} />
-      </Page>
-    </UserProvider>
+    <div className={nunitoSans.className}>
+      <UserProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </UserProvider>
+    </div>
   )
 }
 
