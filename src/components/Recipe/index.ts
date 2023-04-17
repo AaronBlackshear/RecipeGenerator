b951d07daea2d551edcdcb816e44aacc5364bbc0
Aaron Blackshear <ablackshear7820@gmail.com>
2023-04-17 17:37:01 -0600

    Removed old export

--- src/components/Recipe/index.ts
+++ src/components/Recipe/index.ts
@@ -1,4 +1,3 @@
 export * from '@components/Recipe/RecipeCondensed';
 export * from '@components/Recipe/RecipeView';
 export * from '@components/Recipe/Recipe';
-export * from '@components/Recipe/types';
