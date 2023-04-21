9e3e110fe87294abaa5ace6be34832082b163266
Aaron Blackshear <ablackshear7820@gmail.com>
2023-04-20 18:54:57 -0600

    Setup prisma for interacting with db (#18)

--- src/pages/api/recipes/index.ts
+++ src/pages/api/recipes/index.ts
@@ -1,24 +1,13 @@
-import { MongoClient } from 'mongodb'
+import { PrismaClient } from '@prisma/client';
+import { MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// Replace the uri string with your connection string.
-const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${encodeURI(process.env.MONGO_PASSWORD || '')}${process.env.MONGO_CONNECTION_URL}/?retryWrites=true&w=majority`;
+const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const client = new MongoClient(uri);
-  try {
-    const db = client.db('recipe_generator');
-    const recipes = await db
-      .collection("recipes")
-      .find({})
-      .toArray();
-
-    res.status(200).json({ recipes })
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  const recipes = await prisma.recipes.findMany()
+  res.status(200).json({ recipes })
 }
