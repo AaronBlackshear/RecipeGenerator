46be35d5385fee2f74c6be98755884c0e5ba3544
Aaron Blackshear <ablackshear7820@gmail.com>
2023-04-20 18:44:32 -0600

    Setup prisma for interacting with db

--- src/pages/api/recipes/new.ts
+++ src/pages/api/recipes/new.ts
@@ -1,24 +1,15 @@
+import { PrismaClient } from '@prisma/client';
 import { validateRecipeFormat } from '@shared/utils/recipe';
-import { createClient } from '@utils/db';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const client = createClient()
-  try {
-    if (!validateRecipeFormat(req.body.recipe)) res.status(400).json({ message: "Invalid recipe format" })
-    else {
-      const db = client.db('recipe_generator');
-      const recipe = await db
-        .collection("recipes")
-        .insertOne(req.body.recipe)
+  if (!validateRecipeFormat(req.body.recipe)) res.status(400).json({ message: "Invalid recipe format" })
 
-      res.status(200).json({ recipe })
-    }
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  const recipes = await prisma.recipes.create({ data: req.body.recipe });
+  res.status(200).json({ recipes })
 }
